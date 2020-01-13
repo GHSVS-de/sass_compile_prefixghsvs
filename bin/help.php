5f0460357328eb0cae1f77eb00c4bf53364bab13
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2020-01-13 16:03:20 +0100

    didadu

--- bin/help.php
+++ bin/help.php
@@ -10,31 +10,50 @@ $options = getopt("r::");
 if (isset($options['r']))
 {
 	$realPaths = true;
-	$output[] = '### Output of `npm run ghs-help-real-paths` (path variables replaced with values from package.json)';
+	$output[] = '### Output of `npm run g-help-real-paths` (path variables replaced with values from package.json)';
 }
 else
 {
-	$output[] = '### Output of `npm run ghs-help` (path variables not translated)';
+	$output[] = '### Output of `npm run g-help` (path variables not translated)';
 }
 
 $scriptsHelp = array(
-	'What?' => 'Compiles, minifies, autoprefixes *.scss files to CSS and creates source-maps.',
+	'What?' =>
+	'A half-assed, scrawly package for domestic use that compiles, minifies, autoprefixes *.scss files to CSS and creates source-maps.' . NL
+	. '- Can also upload the files via FTP.' . NL
+	. '- Multiple projects/websites can be configured in 1 repository. But only 1 at the same time can be processed!' . NL
+	. '- No luxury. You need to know what you\'re doing.',
+
 	'Tested?' => 'Only with WSL (Windows Subsystem for Linux / DEBIAN) + PHP 7.3 + Node + NPM on local machine.',
-	'Be aware' => 'These npm scripts are very ungraceful. Always have a look on your console messages if the previous job is finished before you fire a new command or save your scss when `nodemon` watchers are active or this or that. Or optmize yourself the scripts for your needs ;-)' . NL .
-	'- The very first usage of `npm run` after opening the WSL console is always slow.', 
+
+	'Be aware' => 'These npm scripts are very ungraceful. Always have a look on your console messages if the previous job is finished before you fire a new command.' . NL
+	. '- The very first usage of `npm run [SCRIPTKEY]` in a console session is always slow. At least with WSL.' . NL
+	. '- Be not so stupid to use it in a productive environment without tests.',
+
+	'How to configure' => 'Create a project folder like `yourOwnProject/` inside main dir of repository.' . NL
+	. '- Create and configure a file `yourOwnProject/package.json`. See example file `myProject/package.json`.' . NL
+	. '- Optional: Create and configure a file `yourOwnProject/ftp-credentials.json` inside project folder. See example file `myProject/ftp-credentials.json`.'
+	. '- Run `node prepareProject.js yourOwnProject` and confirm to override the **main** `package.json`. Afterwards it\'s changed and configured for the project `yourOwnProject`. These keys are adapted:' . NL
+	. '```
+	"versionTxt" (just a datetime stamp)
+	"DIR" > "scss"
+	"DIR" > "target"
+	"DIR" > "project"
+	"DIR" > "projectName"
+	```', 
 	'How to start a script' =>  '`npm run [SCRIPTKEY]`',
-	'The DIR block (inside package.json)' =>
-	'scss: Variable `$npm_package_DIR_scss` (absolute path). The source *.scss-directory.' . NL
-	. '- target: Variable `$npm_package_DIR_target` (absolute path). Dir (normally a template folder) where all the compiled, prefixed, minified CSS files (inside subfolders) are copied to after all these steps are finished.' . NL
-	. '- work: Variable `$npm_package_DIR_work` (relative path). Temporary local work directory inside same folder where `package.json` is located.' . NL
-	. '- css: Variable `$npm_package_DIR_css` (relative path). Dir where all compiled, prefixed, minified CSS files are copied to.' . NL
-	. '- raw:  Variable `$npm_package_DIR_raw` (relative path). Dir where all compiled, minified **BUT NOT PREFIXED** CSS files are copied to.' . NL
-	. '- dist:  Variable `$npm_package_DIR_dist` (relative path). Dir where all compiled, prefixed, minified CSS files are copied to. Source folder for transfer to target and FTP transfer.' .NL
-	. NL . '##### package.json. Current `DIR` config.' . NL . ' ```' . print_r($packageJson['DIR'], true) . '```'
-);
+	'The `DIR` block (inside **main** `package.json`):' =>
+	'Normally you don\'t have to change it yourself if you use `prepareProject.js [PROCECTFOLDER]` before first `npm` usage.' . NL
+	. '- scss: Variable `$npm_package_DIR_scss` (absolute path). The source *.scss-directory.' . NL
+	. '- target: Variable `$npm_package_DIR_target` (absolute path). Dir (normally your template folder) where the whole **content** of folder `$npm_package_DIR_dist` will be transfered to.' . NL
+	. '- project: Variable `$npm_package_DIR_project` (absolute path). Local dir of the project.' . NL
+	. '- work: Variable `$npm_package_DIR_work` (relative path). Temporary local work directory inside active/relevant project folder `$npm_package_DIR_project`.' . NL
+	. '- css: Variable `$npm_package_DIR_css` (relative path). In the beginning a work dir inside folder `$npm_package_DIR_work`. At the end a folder inside `$npm_package_DIR_dist` where all compiled, prefixed, minified CSS/MAP files are located.' . NL
+	. '- raw: Variable `$npm_package_DIR_raw` (relative path). In the beginning a work dir inside folder `$npm_package_DIR_work`. At the end a folder inside `$npm_package_DIR_dist` where all compiled, minified **BUT NOT PREFIXED** CSS files are located.' . NL
+	. '- dist:  Variable `$npm_package_DIR_dist` (relative path). Final source folder for transfer to `$npm_package_DIR_target` and for the FTP transfer. The whole **content** of this folder will be transfered. Not the folder itself.');
 
 $ftpHelp = array(
-	'FTP configuration (inside ftp-credentials.json)' =>
+	'FTP configuration (inside `$npm_package_DIR_project/ftp-credentials.json`)' =>
 	'**No guarantees concerning security!**' . NL
 	. '- See also file `ftp-credentials-example.json`.' . NL
 	. '- place a comment here: Place one if you want to. No usage.' . NL
@@ -42,8 +61,7 @@ $ftpHelp = array(
 	. '- server: The FTP Server/Host.' . NL
 	. '- user: The FTP username.' . NL
 	. '- password: The password of user.' . NL
-	. '- remoteDir: The ftp directory. Starts and ends with a slash (`/`)! If it\'s the FTP ROOT just a single slash.' . NL
-	. '- dir: The local directory No slash (`/`) before and after! All **contents** of this dir will be transfered to `remoteDir`.' . NL
+	. '- remoteDir: The ftp directory. Starts and ends with a slash (`/`)! If it\'s the FTP ROOT of the current FTP connection just a single slash.' . NL
 	. '- ssl: **I have never tested with value `false`!**.' . NL
 	. '- passive: At least `true` on Windows is recommended because of firewall blockades.'
 );
